9e63b3afe0b5adef3d1f4d31fab7eebbe2a055f0
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-08-09 19:40:48 -0500

    covered lifecycle case

--- index.test.js
+++ index.test.js
@@ -6,6 +6,41 @@ b.describe('Classy', function () {
     b.it('can extend prototypes automatically', function (t) {
         var TwoClassy = Classy.extend();
         t.expect(TwoClassy).toBeFunction();
-        t.expect(TwoClassy()).toBeInstance(Classy.constructor);
+        var instance = TwoClassy();
+        t.expect(instance).toBeInstance(Classy.constructor);
+        t.expect(instance).toBeInstance(TwoClassy.constructor);
+    }, 3);
+    b.it('has methods option which attaches properties directly to prototype', function (t) {
+        var TwoClassy = Classy.extend({
+            methods: {
+                methodName: method
+            }
+        });
+        t.expect(TwoClassy().methodName).toBe(method);
+
+        function method() {}
     });
+    b.it('has lifecycle methods which are called in a chain', function (t) {
+        var counter = 0;
+        var TwoClassy = Classy.extend({
+            lifecycle: {
+                created: function (supr, arg) {
+                    t.expect(arg).toBe(1);
+                    supr();
+                }
+            }
+        });
+        var ThreesCompany = TwoClassy.extend({
+            lifecycle: {
+                created: function (supr, opts) {
+                    supr(counter + 1);
+                    t.expect(opts).toBe(options);
+                }
+            }
+        });
+        var options = {};
+        t.expect(ThreesCompany(options).created).toBeUndefined();
+
+        function method() {}
+    }, 3);
 });
